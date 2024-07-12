71372cf5ff703561d947c23a33c34de990377152
ianptkcs <ianptkcs@gmail.com>
2024-07-12 00:50:40 -0300

    Ajuste barra lateral

--- src/components/BarraLateral/index.jsx
+++ src/components/BarraLateral/index.jsx
@@ -22,7 +22,7 @@ const AsideEstilizada = styled.aside`
     background-color: #001634;
     nav {
         position: sticky;
-        top: 13vh;
+        top: 15vh;
     }
 
     @media (max-width: 900px) {
